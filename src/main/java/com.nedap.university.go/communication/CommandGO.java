08ea220dba2a4f8bd12dc7178e74fed703cb271b
Claudia Reuvers <claudia.reuvers@nvc3146.nedap.local>
2017-02-17 09:58:56 +0100

    Add command GO & PLAYER

--- src/main/java/com.nedap.university.go/communication/CommandGO.java
+++ src/main/java/com.nedap.university.go/communication/CommandGO.java
@@ -1,7 +1,78 @@
 package com.nedap.university.go.communication;
 
+import com.nedap.university.go.game.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by claudia.reuvers on 13/02/2017.
  */
-public class CommandGO {
+public class CommandGO implements Command{
+
+    private String incomming;
+    private int size;
+    private ClientHandler client;
+
+    public CommandGO(String line) {
+        incomming = line;
+    }
+
+    @Override
+    public void execute(Server server, ClientHandler client) {
+        this.client = client;
+        if (!checkArguments(server)) {
+            client.sendMessage("WARNING Wrong arguments"); //TODO: adapt
+            return;
+        }
+        if (server.isMatch(size)) {
+            server.removeFromWaitingList(size);
+            ClientHandler opponent = server.getMatch(size);
+            server.setGame(client, opponent, size);
+        } else {
+            server.addToWaitingList(size, client);
+            client.sendMessage("WAITING");
+        }
+
+    }
+
+    @Override
+    //TODO: add warning for wrong arguments
+    public boolean checkArguments(Server server) {
+        String args[] = incomming.split(" ");
+        if (checkArgumentsLength(args)) {
+            try {
+                size = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                client.sendMessage("Not an integer");
+                return false;
+            }
+            return server.checkSize(size);
+        } else {
+            client.sendMessage("Wrong length");
+            return false;
+        }
+    }
+
+    private boolean checkArgumentsLength(String[] args) {
+        if (args.length != 2) {
+            //TODO: throw WrongNrOfArgumentsException
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public List<String> getArguments() {
+        List<String> args = new ArrayList<>();
+        args.add(Integer.toString(size));
+        return args;
+    }
+
+    public void startGame(ClientHandler client1, ClientHandler client2, int size) {
+        Game game = new Game(client1, client2, size);
+        game.start();
+    }
+
+
 }
--- src/main/java/com.nedap.university.go/communication/CommandPLAYER.java
+++ src/main/java/com.nedap.university.go/communication/CommandPLAYER.java
@@ -1,7 +1,64 @@
 package com.nedap.university.go.communication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by claudia.reuvers on 13/02/2017.
  */
-public class CommandPLAYER {
+public class CommandPLAYER implements Command {
+
+    //private Server server;
+    private String incomming;
+    private String name;
+
+    public CommandPLAYER(String line) {
+//        this.server = server;
+        incomming = line;
+    }
+
+    @Override
+    public void execute(Server server, ClientHandler client) {
+        if (!checkArguments(server)) {
+            client.sendMessage("WARNING The name does not meet the requirements, must use a maximum of 20 lowercase letters");
+            //TODO: adapt checkName: s.t. checks if already in list (now only checks if length <= 20 & lowercase)
+            //TODO: sendMessage(WARNING there already is a player with this username, please try another name)
+            return;
+        }
+        client.setClientName(name);
+        server.broadcastToAll("CHAT [Player " + name + " has entered]");
+        client.sendMessage("CHAT Current players:\n" + server.getClientList());
+    }
+
+    @Override
+    public boolean checkArguments(Server server) {
+//        String[] args = incomming.split(" ");
+//        if (args.length != 2) {
+//            //TODO: throw WrongNrOfArgumentsException
+//        } else {
+//            name = args[1];
+//        }
+        String args[] = incomming.split(" ");
+       if (checkArgumentsLength(args)) {
+           name = args[1];
+           return (server.checkName(name));
+       } else {
+           return false;
+       }
+    }
+
+    private boolean checkArgumentsLength(String[] args) {
+        if (args.length != 2) {
+            //TODO: throw WrongNrOfArgumentsException
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public List<String> getArguments() {
+        List<String> args = new ArrayList<>();
+        args.add(name);
+        return args;
+    }
 }
