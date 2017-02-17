0d304964f66992bb8155846c4ec6d9f37c6b9764
Claudia Reuvers <claudia.reuvers@nvc3146.nedap.local>
2017-02-17 09:57:51 +0100

    Add first Commands

---
+++ src/main/java/com.nedap.university.go/communication/CommandGO.java
@@ -0,0 +1,7 @@
+package com.nedap.university.go.communication;
+
+/**
+ * Created by claudia.reuvers on 13/02/2017.
+ */
+public class CommandGO {
+}
---
+++ src/main/java/com.nedap.university.go/communication/CommandREADY.java
@@ -0,0 +1,7 @@
+package com.nedap.university.go.communication;
+
+/**
+ * Created by claudia.reuvers on 17/02/2017.
+ */
+public class CommandREADY {
+}
