99f78ea55e01c4548d31d3f749f993a0a690dfc1
Claudia Reuvers <claudia.reuvers@nvc3146.nedap.local>
2017-02-17 09:59:35 +0100

    Add Command MOVE & READY

--- src/main/java/com.nedap.university.go/communication/CommandMOVE.java
+++ src/main/java/com.nedap.university.go/communication/CommandMOVE.java
@@ -1,7 +1,70 @@
 package com.nedap.university.go.communication;
 
+import com.nedap.university.go.game.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by claudia.reuvers on 13/02/2017.
  */
-public class CommandMOVE {
+public class CommandMOVE implements Command {
+
+    private String incomming;
+    private int x;
+    private int y;
+
+    public CommandMOVE(String line) {
+        incomming = line;
+    }
+
+    @Override
+    public void execute(Server server, ClientHandler client) {
+        if (!checkArguments(server)) {
+            client.sendMessage("WARNING invalid command");
+            return;
+        }
+        Game game = client.getGame();
+        if (server.isValidMove(game, x, y)) {
+            boolean white = true; //TODO: get clientcolor
+            game.getBoard().addStone(x, y, white);
+            //TODO: check if end of game
+            String msg = "MOVED " + " " + booleanToColor(white) + "" + x + " " + y;
+            server.broadcastToGame(game, msg);
+        } else {
+
+        }
+        //TODO: [somewhere] check if it is his/her turn
+    }
+
+    @Override
+    public boolean checkArguments(Server server) {
+        String[] args = incomming.split(" ");
+        if (args.length != 3) {
+            return false;
+        }
+        try {
+            x = Integer.parseInt(args[1]);
+            y = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private String booleanToColor(boolean white) {
+        if (white) {
+            return "white";
+        } else {
+            return "black";
+        }
+    }
+
+    @Override
+    public List<String> getArguments() {
+        List<String> args = new ArrayList<>();
+        args.add(Integer.toString(x));
+        args.add(Integer.toString(y));
+        return args;
+    }
 }
--- src/main/java/com.nedap.university.go/communication/CommandREADY.java
+++ src/main/java/com.nedap.university.go/communication/CommandREADY.java
@@ -1,7 +1,59 @@
 package com.nedap.university.go.communication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by claudia.reuvers on 17/02/2017.
  */
-public class CommandREADY {
+public class CommandREADY implements Command {
+
+    private String incomming;
+    private boolean color;
+    private int size;
+
+    public CommandREADY(String line) {
+        incomming = line;
+    }
+
+    @Override
+    public void execute(Server server, ClientHandler client) {
+        if (checkArguments(server)) {
+
+        }
+
+    }
+
+    @Override
+    public boolean checkArguments(Server server) {
+        String[] args = incomming.split(" ");
+        if (checkArgumentsLength(args)) {
+            String colorString = args[1];
+            if (colorString.equals("white")) {
+                color =  true;
+            } else {
+                color = false;
+            }
+            size = Integer.parseInt(args[2]); //should be right since comes from server and was checked
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean checkArgumentsLength(String[] args) {
+        if (args.length != 3) {
+            //TODO: throw WrongNrOfArgumentsException
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public List<String> getArguments() {
+        List<String> args = new ArrayList<>();
+        args.add(String.valueOf(color));
+        args.add(String.valueOf(size));
+        return args;
+    }
 }
