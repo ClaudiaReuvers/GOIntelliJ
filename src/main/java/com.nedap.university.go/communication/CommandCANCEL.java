f6ad1f011472e213172b57e9113372c4f0e58c2e
Claudia Reuvers <claudia.reuvers@nvc3146.nedap.local>
2017-02-17 09:58:37 +0100

    Add commandCANCEL

--- src/main/java/com.nedap.university.go/communication/CommandCANCEL.java
+++ src/main/java/com.nedap.university.go/communication/CommandCANCEL.java
@@ -1,7 +1,35 @@
 package com.nedap.university.go.communication;
 
+import java.util.List;
+
 /**
  * Created by claudia.reuvers on 13/02/2017.
  */
-public class CommandCANCEL {
+public class CommandCANCEL implements Command {
+
+    private String incomming;
+
+    public CommandCANCEL(String line) {
+        incomming = line;
+    }
+
+    @Override
+    public void execute(Server server, ClientHandler client) {
+        if (checkArguments(server)) {
+            server.removeFromWaitingList(client.getClientSize());
+            server.removeFromClientHandlerList(client);
+            server.broadcastToAll("CHAT [Player " + client.getClientName() + " has left]");
+        }
+    }
+
+    @Override
+    public boolean checkArguments(Server server) {
+        String[] args = incomming.split(" ");
+        return (args.length == 1);
+    }
+
+    @Override
+    public List<String> getArguments() {
+        return null;
+    }
 }
