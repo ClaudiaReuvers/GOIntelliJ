47ec717ea2ee47cdbe03648ac1d46376ff01880a
Claudia Reuvers <claudia.reuvers@nvc3146.nedap.local>
2017-02-20 10:41:14 +0100

    Create ClientCommandTABLEFLIPPED

---
+++ src/main/java/com.nedap.university.go/communication/ClientCommandTABLEFLIPPED.java
@@ -0,0 +1,47 @@
+package com.nedap.university.go.communication;
+
+import java.util.List;
+
+/**
+ * Created by claudia.reuvers on 20/02/2017.
+ */
+public class ClientCommandTABLEFLIPPED implements ClientCommand {
+
+    String incomming;
+
+    public ClientCommandTABLEFLIPPED(String line) {
+        incomming = line;
+    }
+
+    @Override
+    public void execute(Client client) {
+        String[] args = incomming.split(" ");
+        boolean color  = colorToBoolean(args[1]);
+        if (color == client.getColor()) {
+            client.print("You gave up.");
+        } else {
+            client.print("Your opponent gave up.\nYOU WON!!");
+        }
+    }
+
+    private boolean colorToBoolean(String color) {
+        if (color.equals("white")) {
+            return true;
+        } else if (color.equals("black")) {
+            return false;
+        } else {
+            //TODO
+            return false;
+        }
+    }
+
+    @Override
+    public boolean checkArguments() {
+        return false;
+    }
+
+    @Override
+    public List<String> getArguments() {
+        return null;
+    }
+}
